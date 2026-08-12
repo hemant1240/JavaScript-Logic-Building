a29c0b2fa716e968deed49c05055e59016af791c
Hemant <hemantpippal2006@gmail.com>
2026-08-12 20:21:39 +0530

    update

---
+++ areaofrectangle.js
@@ -0,0 +1,7 @@
+function calculateArea(length, width) {
+    let area = length * width;
+    console.log(`The area of rectangle with length ${length} and width ${width} is ${area}.`);
+}
+
+calculateArea(5, 10);
+calculateArea(-5, -10);
