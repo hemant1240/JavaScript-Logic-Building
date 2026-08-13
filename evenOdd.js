89cfcc992cc9f415f990b56f49e858bb287292df
Hemant <hemantpippal2006@gmail.com>
2026-08-13 10:50:41 +0530

    these file is to check a even or odd number

---
+++ evenOdd.js
@@ -0,0 +1,14 @@
+function findEvenOdd(number) {
+  try {
+    if (number % 2 === 0) {
+      console.log("The number is even");
+    } else {
+      console.log("The number is odd");
+    }
+    return number;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+findEvenOdd(ehrhr);
