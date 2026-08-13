f9e0b0ed0311ca086113c8e6268add8a8272a70f
Hemant <hemantpippal2006@gmail.com>
2026-08-13 10:40:37 +0530

    midified

--- areaofrectangle.js
+++ areaofrectangle.js
@@ -1,7 +1,15 @@
 function calculateArea(length, width) {
+
+    if(length <= 0 || width <= 0) {
+        throw new Error("Please enter positive values for length and width.");
+    }
     let area = length * width;
     console.log(`The area of rectangle with length ${length} and width ${width} is ${area}.`);
 }
 
 calculateArea(5, 10);
-calculateArea(-5, -10);
+try {
+    calculateArea(-5, -10);
+} catch (error) {
+    console.error(error.message);
+}
